4679fb7df929d75e8b1be281f5d5950af1c504f8
Tiago Yuga <tiagoteixeira2214@gmail.com>
2017-05-26 12:00:58 -0300

    Subindo projeto pro Git

---
+++ teste128/migrations/0001_initial.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Modelo128',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
+                ('campo1', models.IntegerField()),
+                ('campo2', models.TextField()),
+            ],
+        ),
+    ]
